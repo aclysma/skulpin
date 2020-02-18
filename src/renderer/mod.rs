b4451490e457cd7ad504819a99d978a8d0f38ff4
Keith Simmons <keithsim@microsoft.com>
2020-02-18 10:08:28 -0800

    minor logical size change

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -63,10 +63,14 @@ pub struct LogicalSize {
 impl LogicalSize {
     pub fn new(window: &Window) -> Result<LogicalSize, String> {
         let size = window.vulkan_drawable_size();
+        LogicalSize::from_physical_size_tuple(size, window)
+    }
+
+    pub fn from_physical_size_tuple(tuple: (u32, u32), window: &Window) -> Result<LogicalSize, String> {
         let (hdpi, vdpi) = dpis(window)?;
         Ok(LogicalSize { 
-            width: (size.0 as f32 * DEFAULT_DPI / hdpi) as u32, 
-            height: (size.1 as f32 * DEFAULT_DPI / vdpi) as u32
+            width: (tuple.0 as f32 * DEFAULT_DPI / hdpi) as u32, 
+            height: (tuple.1 as f32 * DEFAULT_DPI / vdpi) as u32
         })
     }
 }
