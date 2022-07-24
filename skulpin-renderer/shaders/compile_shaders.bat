751f8e78e389bc11422643fa78bc3c96c8b85989
Christian Swinehart <drafting@samizdat.co>
2022-07-24 18:37:21 -0400

    use metal backend on macOS

--- skulpin-renderer/shaders/compile_shaders.bat
+++ skulpin-renderer/shaders/compile_shaders.bat
@@ -1,1 +1,2 @@
-rafx-shader-processor --glsl-path glsl/*.vert glsl/*.frag glsl/*.comp --cooked-shaders-path out --package-vk
+rafx-shader-processor --glsl-path glsl --cooked-shaders-path out/vk --package-vk
+rafx-shader-processor --glsl-path glsl --cooked-shaders-path out/mtl --package-metal
